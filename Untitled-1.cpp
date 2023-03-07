5bd0391b8b0007e5df5c31f3bfddef6c6779f6ef
Aparna Das Pias <104611659+gothamknight07@users.noreply.github.com>
2023-03-06 23:48:50 -0800

    changed

---
+++ Untitled-1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    cout<<"Enter a String";
+    getline(cin,s);
+
+    cout<<"You Have Entered:";
+    cout<<s
+
+    s.push_back('Y');
+    cout<<"After Pushback:"<<
+
+    return 0;
+}
